1063a80f8afc4fb4dea9192e5a3ea9b249178ce7
rajshekhorroy <rajshekhorroy@gmail.com>
2021-09-02 23:14:45 -0500

    cutoff added

--- features/intrachain_from_pdb/cutoff_cmap.py
+++ features/intrachain_from_pdb/cutoff_cmap.py
@@ -6,9 +6,10 @@ import numpy as np
 #used to apply cut off to the contact maps in case of predicted contact maps this can be used
 
 in_file = sys.argv[1]
-out_dir = sys.argv[2]
+cut_off = float(sys.argv[2].strip())
+out_dir = sys.argv[3]
 
-cut_off = 0.5
+# cut_off = 0.5
 if not os.path.isfile(in_file):
     print("FILE DOESNT EXISTS ")
     exit()
@@ -20,3 +21,4 @@ out_file = out_dir + name + "_intra.cmap"
 pred_arr[pred_arr <= cut_off] = 0
 pred_arr[pred_arr > cut_off] = 1
 np.savetxt(out_file, pred_arr)
+print( "outfile "+str(out_file))
